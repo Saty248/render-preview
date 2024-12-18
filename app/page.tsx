7557fba2bd3d01ec204d2b8a244e68462b163ba7
Saty248 <mondal.sayantan1234@gmail.com>
2024-12-19 04:01:42 +0530

    secret file read

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 "use client";
+import { readFileSync } from "fs";
 import Image from "next/image";
 
 export default function Home() {
+	const printFile = async () => {
+		let text = "";
+		try {
+			text = readFileSync("/etc/secrets/pvt_key", "utf8");
+		} catch {
+			console.log("error");
+		}
+		return text;
+	};
+
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -27,8 +38,9 @@ export default function Home() {
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<button
 						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 text-black bg-white"
-						onClick={() => {
-							console.log("env", process.env.NEXT_PUBLIC_API_KEY);
+						onClick={async () => {
+							const text = await printFile();
+							console.log("env", text);
 						}}
 						rel="noopener noreferrer"
 					>
